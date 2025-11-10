edafbc40a95b810d04458abdb38d25612b7665ef
lipengpeng <lipengpeng@7k7k.com>
2025-11-10 18:05:12 +0800

    udpate

--- js/games.js
+++
@@ -1,5 +0,0 @@
-// 全站统一游戏配置
-const games = [
-    { name: "打地鼠", folder: "whack-a-mole", img: "games/whack-a-mole/cover.jpg" },
-    { name: "贪吃蛇", folder: "snake", img: "games/snake/cover.jpg" }
-];
